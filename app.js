24f96f127f9dc268ee3d2fdf7dc5839c0a064860
MichaelLake01 <michael.lake.av18@gmail.com>
2023-02-17 13:20:38 +0000

    Added functionality to create a calculator interface

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+const calContainer = document.querySelector("#calc-container");
+const numPad = document.querySelector("#num-pad");
+const operandContainer= document.querySelector("#operand-container");
+const optionsContainer = document.querySelector("#options-container");
+
+innit();
+
+function innit()
+{
+    createNumPad();
+    createOperands();
+}
+
+function createNumPad()
+{  
+    for (let i = 0; i < 10; i++) {
+        const num = document.createElement("button");
+        num.classList = ["num", `num${i}`];
+        num.textContent = i;
+        numPad.append(num);
+    }
+}
+
+function createOperands()
+{
+    const operands = ["+", "-", "*", "/"];
+    for (let i = 0; i < operands.length; i++) {
+        const operand = document.createElement("button");
+        operand.classList = ["operand", operands[i]];
+        operand.textContent = operands[i];
+        operandContainer.append(operand);
+    }
+}
+
 
 
 
